e5f20ac8695a091ff1d9c4a7642b33d68885d4b5
Pritam Singh <pritam.singh@intelliswift.com>
2025-07-23 13:50:25 +0530

    Add file with multiple JavaScript syntax errors

---
+++ test-github-syntax-error.js
@@ -0,0 +1,7 @@
+﻿const brokenFunction = () => {
+  console.log("This is missing a closing bracket and semicolon"
+  let x = 5
+  if (x > 3 {
+    console.log("Missing closing parenthesis"
+  }
+  // Multiple syntax errors above
