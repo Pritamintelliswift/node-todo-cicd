1980a4fb90d9b353b656da89314a554deff89a3a
Pritam Singh <pritam.singh@intelliswift.com>
2025-07-23 14:07:02 +0530

    Add JS file with intentional syntax errors for webhook test

---
+++ broken-syntax-test.js
@@ -0,0 +1,7 @@
+﻿const badFunction = () => {
+  console.log("This line has an unclosed parenthesis"
+  const a = 10
+  if (a == 10 {
+    console.log("Another syntax mistake"
+  }
+  // Errors include: missing closing brackets, missing semicolons, unbalanced parentheses
